5523798682c35184d399877c91ee217262e64308
Nathan Chan <nathankcchan@gmail.com>
2017-09-12 22:16:03 -0700

    Remove useless board flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,6 +67,4 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TW_THEME := portrait_hdpi
 TW_INCLUDE_CRYPTO := true
 RECOVERY_SDCARD_ON_DATA := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_HAS_NO_MISC_PARTITION := true
 TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
