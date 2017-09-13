175cbaaddfa99847a7244092dd4d69d126751389
Nathan Chan <nathankcchan@gmail.com>
2017-09-12 22:15:45 -0700

    Fix backlight brightness path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,4 +69,4 @@ TW_INCLUDE_CRYPTO := true
 RECOVERY_SDCARD_ON_DATA := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_NO_MISC_PARTITION := true
-TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness\"
+TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
